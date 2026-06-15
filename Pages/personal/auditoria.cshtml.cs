92ea7e26bbd7cfca0c78693013efc1fcba73f14c
aradan04 <aradanichi@gmail.com>
2026-06-15 00:16:34 -0600

    vista personal completa

---
+++ Pages/personal/auditoria.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyectoIFK.Pages.personal
+{
+    public class AuditoriaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/personal/control.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyectoIFK.Pages.personal
+{
+    public class ControlModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
