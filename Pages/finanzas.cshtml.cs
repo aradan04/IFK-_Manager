051528af235663e6902054621d8ef243f34ca03a
Jesus <jesuselpuppet@outlook.com>
2026-06-22 09:30:10 -0600

    feat: formulario finanzas con buscador, calculos automaticos y estilos

--- Pages/finanzas.cshtml.cs
+++ Pages/finanzas.cshtml.cs
@@ -1,10 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using ProyectoIFK.Data;
 
 namespace ProyectoIFK.Pages;
 
 public class FinanzasModel : PageModel
 {
-    public void OnGet()
+    private readonly ApplicationDbContext _context;
+
+    public FinanzasModel(ApplicationDbContext context)
     {
+        _context = context;
+    }
+
+    public void OnGet() { }
+
+    public JsonResult OnGetBuscarAlumnos(string query)
+    {
+        var resultados = _context.Alumno
+            .Where(a => a.NombreCompleto.Contains(query) || a.Matricula.Contains(query))
+            .Take(5)
+            .Select(a => new { 
+                matricula = a.Matricula, 
+                nombre = a.NombreCompleto 
+            })
+            .ToList();
+
+        return new JsonResult(resultados);
     }
 }
